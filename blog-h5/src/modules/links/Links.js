06cb1d6fdce7633788c2ee64ea156c763612d139
vic <qibei.shan@kakatrip.cn>
2018-11-28 17:36:17 +0800

    站点推荐 update 

--- blog-h5/src/modules/links/Links.js
+++ blog-h5/src/modules/links/Links.js
@@ -17,7 +17,9 @@ class Links extends React.Component {
                 <div className="white-bg">
                     <h2 className="h-title"><span className="h-nav">可能，你觉得自己没有足够得优秀，但我始终相信你仍然在努力坚持着!</span>站长推荐</h2>
                     <ul className="site-tj">
-                        <li><a href="http://xiaao.tk/" target="_blank">夏同学的Blog</a></li>
+                        <li><a href="http://www.xiaao.net.cn" target="_blank">夏同学的Blog</a></li>
+                        <li><a href="https://www.fanchaoyang.com/" target="_blank">点点code</a></li>
+                        <li><a href="http://emojime.cn/" target="_blank">emojime</a></li>
                     </ul>
                 </div>
                 <div className="white-bg">
